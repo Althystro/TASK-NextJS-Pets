5c50285008b69bf339935242df3f385cfac91de9
Abdulwahab-Alawadhi <abdulwahab.alawadhi56@gmail.com>
2024-10-16 12:36:36 +0300

    done

--- src/app/Components/PetItem.js
+++ src/app/Components/PetItem.js
@@ -1,17 +1,31 @@
+"use client";
 import Image from "next/image";
+import { useState } from "react";
 
-const btnStyle = "m-4 p-2 bg-palette-primary text-white rounded-sm font-primary font-semibold  hover:bg-palette-dark"
+const btnStyle =
+  "m-4 p-2 bg-palette-primary text-white rounded-sm font-primary font-semibold  hover:bg-palette-dark";
 
 function PetItem({ pet }) {
+  const [pic, setPic] = useState(pet.image);
+
+  const changePic = () => {
+    if (pic === pet.image) {
+      setPic(pet.image2);
+    } else {
+      setPic(pet.image);
+    }
+  };
+
   return (
     <div className="h-120 w-72 rounded shadow-lg mx-auto border border-palette-lighter">
       <div className="h-72 border-b-2 border-palette-lighter relative">
         <Image
           alt={pet.name}
-          src={pet.image}
+          src={pic}
           className="transform duration-500 ease-in-out hover:brightness-75"
           fill
-          sizes="30vw" />
+          sizes="30vw"
+        />
       </div>
       <div className="h-48 relative">
         <div className="font-primary text-palette-primary text-2xl pt-4 px-4 font-semibold">
@@ -21,7 +35,7 @@ function PetItem({ pet }) {
           {pet.description}
         </div>
         <div className="grid grid-cols-2 gap-2">
-          <button type="button" className={btnStyle}>
+          <button onClick={changePic} type="button" className={btnStyle}>
             Pet
           </button>
           <button type="button" className={btnStyle}>
--- src/app/Components/PetsList.js
+++ src/app/Components/PetsList.js
@@ -1,15 +1,42 @@
+"use client";
+import { useState } from "react";
 import pets from "../data/pets";
 import PetItem from "./PetItem";
 
 function PetsList() {
-  const petList = pets.map((pet) => <PetItem pet={pet} key={pet.id} />);
+  const [query, setQuery] = useState("");
+  const [type, setType] = useState("");
 
+  const petList = pets
+    .filter((pet) => {
+      if (type === "") {
+        return true;
+      } else return pet.type === type;
+    })
+    .filter((pet) => pet.name.toLowerCase().includes(query.toLowerCase()))
+    .map((pet) => <PetItem pet={pet} key={pet.id} />);
+
+  const searchPet = () => {
+    setQuery(event.target.value);
+  };
+  const searchType = (e) => {
+    setType(e.target.value);
+  };
+
+  // const filterPetName = pets.filter((pet) =>
+  //   pet.name.toLowerCase().includes(query.toLowerCase())
+  // );
+
+  // const filtePetSpecies = pets.filter((pet) => {
+  //   pet.type == type;
+  // });
   return (
     <>
       <div className="mx-auto">
         <div className="flex justify-start space-x-2 w-full font-primary">
           <div className="flex flex-col items-start space-y-1 flex-grow">
             <input
+              onChange={searchPet}
               type="search"
               placeholder="search"
               className="text-gray-900 form-input border border-gray-300 w-full rounded-sm focus:border-palette-light focus:ring-palette-light"
@@ -17,7 +44,8 @@ function PetsList() {
           </div>
           <div className="flex flex-col items-start space-y-1 flex-grow-0">
             <select
-              defaultValue={""}
+              onChange={searchType}
+              defaultValue={type}
               className="form-select border border-gray-300 rounded-sm w-full text-gray-900 focus:border-palette-light focus:ring-palette-light"
             >
               <option value="">All</option>
@@ -28,7 +56,9 @@ function PetsList() {
           </div>
         </div>
       </div>
-      <div className="py-12 max-w-6xl mx-auto grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-x-4 gap-y-8">{petList}</div>
+      <div className="py-12 max-w-6xl mx-auto grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-x-4 gap-y-8">
+        {petList}
+      </div>
     </>
   );
 }
